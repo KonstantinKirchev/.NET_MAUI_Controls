900ebbf720270d5cfff0313aa69fb823717b3b1d
konstantinkirchev <konstantin1230@yahoo.com>
2023-12-05 16:46:03 +0200

    Add swipe view with swipe items on left and right side

--- .NET_MAUI_Controls/Pages/ActionControls.xaml.cs
+++ .NET_MAUI_Controls/Pages/ActionControls.xaml.cs
@@ -21,4 +21,24 @@ public partial class ActionControls : ContentPage
 	{
         DisplayAlert("Coding Droplets", $"You've searched for \"{demoSearchBar.Text}\"", "OK");
     }
+
+    private void facebookSwipeItem_Invoked(object sender, EventArgs e)
+    {
+        DisplayAlert("Coding Droplets", "You've clicked the Facebook button", "OK");
+    }
+
+    private void instagramSwipeItem_Invoked(object sender, EventArgs e)
+    {
+        DisplayAlert("Coding Droplets", "You've clicked the Instagram button", "OK");
+    }
+
+    private void twitterSwipeItem_Invoked(object sender, EventArgs e)
+    {
+        DisplayAlert("Coding Droplets", "You've clicked the Twitter button", "OK");
+    }
+
+    private void linkedInSwipeItem_Invoked(object sender, EventArgs e)
+    {
+        DisplayAlert("Coding Droplets", "You've clicked the LinkedIn button", "OK");
+    }
 }
